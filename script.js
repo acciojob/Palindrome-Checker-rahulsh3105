b55ef66675f4b5c2df25735a042a4006bdeab2ce
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-01 10:36:26 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 // complete the given function
 
-function palindrome(str){
-
+function palindrome(s){
+	s = ''.join(filter(str.isalnum, s.lower()))
+    
+    # Check if the string is equal to its reverse
+    return s == s[::-1]
 }
 module.exports = palindrome
