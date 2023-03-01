24f7858485dc746c2421e2e5597d0fc99402a084
Rahul Sharma <rahulsh3105@gmail.com>
2023-03-01 10:39:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,14 @@
 // complete the given function
 
-function palindrome(s){
-	s = ''.join(filter(str.isalnum, s.lower()))
-    
-    # Check if the string is equal to its reverse
-    return s == s[::-1]
+function palindrome(str){
+	const len = str.length;
+
+       for (let i = 0; i < len / 2; i++) {
+
+           if (str[i] !== str[len - 1 - i]) {
+               console.log(false);
+           }
+       }
+       return console.log(true);
 }
 module.exports = palindrome
